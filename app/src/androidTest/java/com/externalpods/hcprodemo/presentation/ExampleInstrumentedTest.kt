a7a02d658cb13f5738e129db0e96890d23a9b1b5
Ramzes Rodriguez <ramzesrodriguez@hotmail.com>
2022-09-24 08:22:20 -0500

    Update test sample

--- app/src/androidTest/java/com/externalpods/hcprodemo/presentation/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/externalpods/hcprodemo/presentation/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
   fun useAppContext() {
     // Context of the app under test.
     val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-    assertEquals("com.externalpods.hcprodemo.presentation", appContext.packageName)
+    assertEquals("com.externalpods.hcprodemo.dev", appContext.packageName)
   }
 }
