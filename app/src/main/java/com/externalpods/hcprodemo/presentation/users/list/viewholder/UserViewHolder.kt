edd6037904e20840ab91d71b4bc480586903297b
Ramzes Rodriguez <ramzesrodriguez@hotmail.com>
2022-09-26 18:21:05 -0500

    update styles

--- app/src/main/java/com/externalpods/hcprodemo/presentation/users/list/viewholder/UserViewHolder.kt
+++ app/src/main/java/com/externalpods/hcprodemo/presentation/users/list/viewholder/UserViewHolder.kt
@@ -1,9 +1,9 @@
 package com.externalpods.hcprodemo.presentation.users.list.viewholder
 
 import androidx.recyclerview.widget.RecyclerView
+import com.adwardstark.mtextdrawable.MaterialTextDrawable
 import com.externalpods.hcprodemo.presentation.databinding.UserItemViewBinding
 import com.externalpods.hcprodemo.presentation.users.list.models.UserModel
-import com.externalpods.hcprodemo.presentation.utils.text.TextToDrawable
 
 class UserViewHolder(
   private val view: UserItemViewBinding
@@ -13,7 +13,7 @@ class UserViewHolder(
     view.textViewAddress.text = item.address?.city
     view.textViewEmail.text = item.email?.lowercase()
     item.name?.let {
-      TextToDrawable.with(view.root.context)
+      MaterialTextDrawable.with(view.root.context)
         .text(it)
         .into(view.userImage)
     }
